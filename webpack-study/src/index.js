8c5fd4d720047c5281cee6e391b188bf80e6e404
seb5144 <seb@woowahan.com>
2018-08-11 17:01:26 +0900

    webpack study 코드 푸시

---
+++ webpack-study/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import message from "./message";
+import Hello from "./Hello"
+ReactDOM.render(<Hello/>, document.getElementById("root"));
+message();
---
+++ webpack-study/webpack.config.js
@@ -0,0 +1,33 @@
+console.log(process.env.NODE_ENV)
+const path = require('path')
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+module.exports = {
+  entry: "./src/index.js",
+  mode: process.env.NODE_ENV == 'development' ? 'development': 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins:[ new HtmlWebpackPlugin({
+    template: 'public/index.html'
+  })],
+  devServer:{
+    contentBase: path.join(__dirname,'public')
+  }
+};
